bfa7e7036a283121c7e3ad710765ef53175b695e
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2015-11-11 11:39:42 +0200

    Working extended mind client and public data classes.

--- index.js
+++ index.js
@@ -3,9 +3,13 @@
 const koaNunjucks = require('./lib/koa-nunjucks-2.js');
 const koaRoute = require('./lib/koa-route.js');
 const koaSendfile = require('./lib/koa-sendfile.js');
+const extendedmind = require('./lib/extendedmind.js');
+const extendedmindData = require('./lib/extendedmind-data.js');
 
 exports = module.exports = {
   koaNunjucks: koaNunjucks,
   koaRoute: koaRoute,
-  koaSendfile: koaSendfile
+  koaSendfile: koaSendfile,
+  extendedmind: extendedmind,
+  extendedmindData: extendedmindData
 }
