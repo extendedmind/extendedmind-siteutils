ce99813cdc13f7190333efc2c7f1776372ee43fa
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2016-02-12 11:06:45 +0200

    Fixed cache updating all the time and not respecting one minute frequency, cache is now reset after UTC midnight, when unpublished are removed from ?modified response. Removed koa-route as 3.0.0 official is released

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 'use strict'
 
 const koaNunjucks = require('./lib/koa-nunjucks-2.js');
-const koaRoute = require('./lib/koa-route.js');
 const koaSendfile = require('./lib/koa-sendfile.js');
 const extendedmind = require('./lib/extendedmind.js');
 const extendedmindData = require('./lib/extendedmind-data.js');
 
 exports = module.exports = {
   koaNunjucks: koaNunjucks,
-  koaRoute: koaRoute,
   koaSendfile: koaSendfile,
   extendedmind: extendedmind,
   extendedmindData: extendedmindData
